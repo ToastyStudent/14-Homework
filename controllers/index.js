1ffafbc65ccafe399dd19b7c0ac6bd51f0248895
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-09 15:55:32 -0500

    Added comments to the index.js file in the controllers folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,14 @@
+// Dependencies
 const router = require('express').Router();
 
+// Imports the various routes from the "api" directory and the "home-routes.js" and "dashboard-routes.js" files
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+
+// Adds the prefix of `/` to all of the home routes imported from the "home-routes.js" file 
+// Adds the prefix of `/dashboard` to all of the dashboard routes imported from the "dashboard-routes.js" file
+// Adds the prefix of`/api` to all of the api routes imported from the "api" directory
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
