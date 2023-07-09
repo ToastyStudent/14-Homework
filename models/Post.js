16d47c02aa10f15cddc6792fdddc57da701cff4f
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-09 15:27:33 -0500

    Added comments into Post.js file

--- models/Post.js
+++ models/Post.js
@@ -5,6 +5,10 @@ class Post extends Model {}
 
 Post.init(
   {
+    // Definition of Collumns for the Post Model, named title and body respectively
+    
+    // title is a string and cannot be null
+    // body is a string and cannot be null
     title: DataTypes.STRING,
     body: DataTypes.STRING
   },
