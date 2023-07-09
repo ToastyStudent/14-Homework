fb60ac006a635f8c0fa74656bc61c7aeee3cb128
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-09 15:12:02 -0500

    Defined the relationship between models in the index.js file

--- models/index.js
+++ models/index.js
@@ -1,22 +1,35 @@
+// Import Models
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// The following code defines the relationship between each of the models
+// and one another
+
+// Post belongsTo User, meaning that the Post model 
+// has a foreign key in the form of "userId" that references the
+// User model's primary key of "id"
 Post.belongsTo(User, {
   foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
+// Posts have many Comments, meaning that a single Post can have 
+// multiple Comments and that each one of said Comments has a foreign key
+// in the form of "postId" that references the Post model's primary key of "id"
 Post.hasMany(Comment, {
   foreignKey: 'postId',
   onDelete: 'CASCADE'
 });
 
+// Comments belong to User, meaning that the Comment model has a foreign key in 
+// the form of "userId" that references the User model's primary key of "id"
 Comment.belongsTo(User, {
   foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
+// Exports the User, Post, and Comment models for other parts of the application to use
 module.exports = {
   User,
   Comment,
