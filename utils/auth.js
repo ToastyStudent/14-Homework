b06d3793a1429d41477625f1580b808b8cadb0c6
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-09 16:25:31 -0500

    Added comments into the auth.js file

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,5 @@
+// Javascript used for authentication
+// Id user is not logged in, redirect to login page
 const withAuth = (req, res, next) => {
   if (!req.session.userId) {
     res.redirect("/login");
